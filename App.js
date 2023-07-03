0331db3da128b5929c5947b6f8f266704ba3839a
HunterLL <hunter_law@outlook.com>
2023-07-03 16:50:10 -0700

    session2 changed

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World!🤬 🫡 </Text>
+      <Text>🤬 🫡 </Text>
       <StatusBar style="auto" />
     </View>
   );
