50844ca344b6159149af668f9ee9226742d50348
HunterLL <hunter_law@outlook.com>
2023-07-03 16:54:51 -0700

    emoji changed

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>🤬 🫡 </Text>
+      <Text>🤬🤬🤬🤬🤬🤬🤬🤬 🫡 </Text>
       <StatusBar style="auto" />
     </View>
   );
